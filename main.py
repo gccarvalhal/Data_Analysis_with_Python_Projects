f125783ce62eacf2d3652d87fa8805cb6eca169f
Gabriel Carvalhal <95027395+gccarvalhal@users.noreply.github.com>
2022-09-16 14:12:16 -0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-# This entrypoint file to be used in development. Start by reading README.md
-import mean_var_std
-from unittest import main
-
-l = [0,1,2,3,4,5,6,7,8]
-print(mean_var_std.calculate(l))
-
-# Run unit tests automatically
-main(module='test_module', exit=False)
